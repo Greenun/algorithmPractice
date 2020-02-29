5c873982145af96ef9aa1b1919c4b2bd82edf4e6
Greenun <iopuy0234@gmail.com>
2020-02-29 16:05:51 +0900

    parametric search problem

---
+++ programmers/paper_please.py
@@ -0,0 +1,17 @@
+def solution(n, times):
+    max_time = max(times)
+    upper = max_time * n
+    lower = 0
+    # time t --> done: (t // all time)
+    while lower < upper:
+        middle = (lower + upper) // 2
+        done = sum([(middle // t) for t in times])
+        if done < n:
+            lower = middle + 1
+        else:
+            upper = middle
+    done = sum([(lower // t) for t in times])
+    while done < n:
+        lower += 1
+        done = sum([(lower // t) for t in times])
+    return lower
