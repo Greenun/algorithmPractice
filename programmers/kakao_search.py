98b38704f82c949137df245f3ab92c7652081b1a
Greenun <iopuy0234@gmail.com>
2020-03-25 18:13:17 +0900

    lv3 str problem clear

--- programmers/kakao_search.py
+++ programmers/kakao_search.py
@@ -1,32 +1,37 @@
+# meta tag나 a태그에서 뭔가 함정 부분이 있었나봄..;;
 def solution(word, pages):
     import re
-    url_filter = re.compile(r'<meta property="og:url" content="(.*)"/>')
-    body_filter  = re.compile(r'<body>(.*)</body>', re.DOTALL)
-    link_filter = re.compile(r'<a href="(.*)">')
-    word_filter = re.compile(f'[a-zA-Z]*{word.lower()}[a-zA-Z]*')
+    # url_filter = re.compile(r'<meta property="og:url" content="(.*)"/>')
+    # body_filter  = re.compile(r'<body>(.*)</body>', re.DOTALL)
+    # link_filter = re.compile(r'<a href="(.*)">')
+    url_filter = re.compile(r'<meta(.+?)/>')
+    link_filter = re.compile(r'<a(.+?)>')
+    detail_link = re.compile(r'"(.*)"')
+    word_filter = re.compile(f'[a-z]*{word.lower()}[a-z]*')
     
     page_links = list()
     external_links = list()
     basic_score = list()
     for page in pages:
-        page_links.append(url_filter.search(page).group(1))
-        external_links.append(link_filter.findall(page))    
-        body = body_filter.search(page).group(1).lower()
-        words = word_filter.findall(body)
-        count = 0
-        # 4 6 8 10 17
-        for w in words:
-            if w == word.lower():
-                count += 1
-        basic_score.append(count)
+        page = page.lower()
+        page_links.append(detail_link.search(url_filter.search(page).group(1).split(' ')[2]).group(1))
+        external_links.append([detail_link.search(l).group(1) for l in link_filter.findall(page)])    
+        # body = body_filter.search(page).group(1)
+        # words = [w for w in word_filter.findall(body) if w == word.lower()]
+        # words_count = re.sub(r'[^a-z]+', '.', page).split('.').count(word.lower())
+        words = [w for w in word_filter.findall(page) if w == word.lower()]
+        basic_score.append(len(words))
+        # basic_score.append(words_count)
+    
     final_scores = list()
     for i, link in enumerate(page_links):
-        score = basic_score[i]
-        temp = 0
+        score = float(basic_score[i])
+        temp = 0.0
         for j, external in enumerate(external_links):
+            if i == j: continue
             if link in external:
                 temp += basic_score[j] / len(external)
         score += temp
-        final_scores.append(score)
+        final_scores.append(score)    
+    
     return final_scores.index(max(final_scores))
-        
