e182b293e8073d601a6fc4404eefe962e90397bb
wessup <iopuy0234@gmail.com>
2020-10-05 15:50:24 +0900

    boj gold 3 dp - add DFS logic

--- coalgo/greedy_panda.py
+++ coalgo/greedy_panda.py
@@ -2,22 +2,27 @@ import sys
 from collections import deque, defaultdict
 import heapq
 
+sys.setrecursionlimit(300000)
+
 def solution(n, forest):
-	point_map = defaultdict(list)
-	ordered = list()
-	table = [[1]*n for _ in range(n)]
+	answer = 0
+	global table
+	table = [[0]*n for _ in range(n)]
 	for i in range(n):
 		for j in range(n):
-			point_map[forest[i][j]].append((i, j))
-			heapq.heappush(ordered, forest[i][j])
-	while ordered:
-		current = heapq.heappop(ordered)
-		cx, cy = point_map[current].pop()
-		adjs = [(x, y) for x, y in [(cx+1, cy), (cx-1, cy), (cx, cy-1), (cx, cy+1)] if 0 <= x < n and 0 <= y < n]
-		for ax, ay in adjs:
-			if forest[ax][ay] > forest[cx][cy]:
-				table[ax][ay] = max(table[ax][ay], table[cx][cy] + 1)
-	return max([max(line) for line in table])
+			answer = max(answer, DFS(forest, n, i, j))
+	return answer
+
+def DFS(forest, n, x, y):
+	if table[x][y]:
+		return table[x][y]
+	# print(table)
+	table[x][y] = 1
+	adjs = [(ax, ay) for ax, ay in [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)] if 0 <= ax < n and 0 <= ay < n]
+	for ax, ay in adjs:
+		if forest[ax][ay] < forest[x][y]:
+			table[x][y] = max(table[x][y], DFS(forest, n, ax, ay) + 1)
+	return table[x][y]
 
 if __name__ == '__main__':
 	n = int(sys.stdin.readline().strip())
@@ -64,6 +69,23 @@ if __name__ == '__main__':
 # 	while ordered:
 # 		current = heapq.heappop(ordered)
 # 		cx, cy = point_map[current].pop(0)
+# 		adjs = [(x, y) for x, y in [(cx+1, cy), (cx-1, cy), (cx, cy-1), (cx, cy+1)] if 0 <= x < n and 0 <= y < n]
+# 		for ax, ay in adjs:
+# 			if forest[ax][ay] > forest[cx][cy]:
+# 				table[ax][ay] = max(table[ax][ay], table[cx][cy] + 1)
+# 	return max([max(line) for line in table])
+
+# def solution(n, forest):
+# 	point_map = defaultdict(list)
+# 	ordered = list()
+# 	table = [[1]*n for _ in range(n)]
+# 	for i in range(n):
+# 		for j in range(n):
+# 			point_map[forest[i][j]].append((i, j))
+# 			heapq.heappush(ordered, forest[i][j])
+# 	while ordered:
+# 		current = heapq.heappop(ordered)
+# 		cx, cy = point_map[current].pop()
 # 		adjs = [(x, y) for x, y in [(cx+1, cy), (cx-1, cy), (cx, cy-1), (cx, cy+1)] if 0 <= x < n and 0 <= y < n]
 # 		for ax, ay in adjs:
 # 			if forest[ax][ay] > forest[cx][cy]:
